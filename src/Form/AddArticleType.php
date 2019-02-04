4adb675172186ce5eaeba048ed3937d701cc9de1
Williwill72 <dufour.william72@gmail.com>
2019-02-04 11:19:39 +0100

    correct display on article create page

--- src/Form/AddArticleType.php
+++ src/Form/AddArticleType.php
@@ -3,10 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Article;
-use Doctrine\DBAL\Types\FloatType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,9 +15,15 @@ class AddArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('description',TextareaType::class)
-            ->add('price')
+            ->add('name', TextType::class,[
+                "label" => "nom"
+            ])
+            ->add('description',TextareaType::class,[
+                "label" => "description"
+            ])
+            ->add('price', NumberType::class,[
+                "label" => "prix"
+            ])
         ;
     }
 

96a83e9eade5905a098fe57fcf9b2b3bbe05ab2a
Williwill72 <dufour.william72@gmail.com>
2019-02-04 12:04:54 +0100

    display articles on home page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +13,12 @@ class DefaultController extends AbstractController
      */
     public function home()
     {
-        return $this->render("default/home.html.twig");
+        $articleRepository =$this->getDoctrine()->getRepository(Article::class);
+
+        $articles = $articleRepository->findAll();
+
+        return $this->render("default/home.html.twig",[
+            "articles" => $articles
+        ]);
     }
 }
